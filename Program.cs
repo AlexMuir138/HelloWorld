d522c9dfa33296d97e92121d1982f7bfde204a92
Alex <muirkat138@gmail.com>
2021-09-30 21:37:38 -0600

    next

--- Program.cs
+++ Program.cs
@@ -43,6 +43,16 @@ namespace HelloWorld
 
             // Print the story to the console
             Console.WriteLine(story);
+
+            // Create password
+            string password = "a92301j2add";
+
+            // Get password length
+            int passwordLength = password.Length;
+            // Check if password uses symbol
+            int passwordCheck = password.IndexOf("!");
+            // Print results
+            Console.WriteLine($"The user password is {password}. Its length is {passwordLength} and it receives a {passwordCheck} check.");
         }
     }
 }
