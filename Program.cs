9aa7ff59d7c48d6016c33a67c2231473edcc0d98
Alex <muirkat138@gmail.com>
2021-10-05 14:02:23 -0600

    else if

--- Program.cs
+++ Program.cs
@@ -88,7 +88,16 @@ namespace HelloWorld
                 Console.WriteLine("SaladMart");
             }else{
                 Console.WriteLine("Soup N Sandwich");
-      }
+            }
+
+            int guests = 0;
+            if(guests >= 4){
+                Console.WriteLine("Catan");
+            }else if(guests >= 1){
+                Console.WriteLine("Innovation");
+            }else{
+                Console.WriteLine("Solitaire");
+            }
 
 
         }
