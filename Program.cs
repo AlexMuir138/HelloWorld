57970640d4275d8ce9365e560ffde9e151c49080
Alex <muirkat138@gmail.com>
2021-10-05 13:13:48 -0600

    bools

--- Program.cs
+++ Program.cs
@@ -63,6 +63,15 @@ namespace HelloWorld
             int charPosition = name.IndexOf("Abbasi");
             string lastName = name.Substring(charPosition);
 
+            //Logic
+            double timeToDinner = 4;
+            double distance = 95;
+            double rate = 30;
+            double tripDuration = distance / rate;
+
+            bool answer = (tripDuration <= timeToDinner);
+            Console.WriteLine(answer);
+
 
         }
     }
