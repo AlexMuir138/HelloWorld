9e5f1f81c559a84836a44fc971870d37cd46a43d
Alex <muirkat138@gmail.com>
2021-10-13 12:54:49 -0600

    errors

--- Program.cs
+++ Program.cs
@@ -196,6 +196,32 @@ namespace HelloWorld
                 return $"Welcome to {name}";
                 }
 
+                //ERRORS
+                static void Main(string[] args)
+                {
+                Console.WriteLine(DecoratePlanet("Mars")); 
+                Console.WriteLine("Is Pluto really a dwarf...?");
+                Console.WriteLine(IsPlutoADwarf());
+                Console.WriteLine("Then how many planets are there in the galaxy...?");
+                Console.WriteLine(CountThePlanets());
+                }
+                
+                static string DecoratePlanet(string planet)
+                {
+                return $"*..*..* Welcome to {planet} *..*..*";
+                }
+                
+                static bool IsPlutoADwarf()
+                {
+                bool answer = true;
+                return answer;
+                }
+                
+                static string CountThePlanets()
+                {
+                return "8 planets, usually";
+                }
+
 
         }
     }
